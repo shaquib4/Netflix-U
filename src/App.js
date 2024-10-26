3485727c527f25dda091458b16c1144769274d2c
Mohd Shaquib Khan <mohammadshaquib4@gmail.com>
2024-10-26 12:04:07 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <img className="w-800 h-80" src="https://cdn.pixabay.com/photo/2019/10/28/09/02/kids-4583785_960_720.jpg"></img>
+      <h1 className="text-amber-700 text-2xl font-extrabold text-center my-20">
+        Starting with project
+      </h1>
+    </div>
+  );
+}
+
+export default App;
